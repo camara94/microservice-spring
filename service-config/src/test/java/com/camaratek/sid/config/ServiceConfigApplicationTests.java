36abcba5c0c9783a5f74e1d77204c65cfa9684e2
camara94 <ldamaro98@gmail.com>
2020-06-07 01:51:28 +0200

    creation du service de configuration du microservice

---
+++ service-config/src/test/java/com/camaratek/sid/config/ServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camaratek.sid.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
