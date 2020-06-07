708364f30516557d37de776a18efa358b8a7d845
camara94 <ldamaro98@gmail.com>
2020-06-07 12:29:11 +0200

    creation du service(api) proxy

--- service-company/src/main/java/com/camaratek/sid/company/ServiceCompanyApplication.java
+++ service-company/src/main/java/com/camaratek/sid/company/ServiceCompanyApplication.java
@@ -5,11 +5,13 @@ import java.util.stream.Stream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.camaratek.sid.company.dao.CompanyRepository;
 import com.camaratek.sid.company.models.Company;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceCompanyApplication {
 
